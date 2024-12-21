d82be76f205fb19048959d2afd7ea917a1f95d2c
MiguelPalafox17 <ma.palafox23@info.uas.edu.mx>
2024-12-21 00:47:55 -0600

    Imagenes Extra

--- src/Galeria/Galeria.jsx
+++ src/Galeria/Galeria.jsx
@@ -9,6 +9,12 @@ import Imagen6 from './Imagenes Galeria/Imagen 6.png'
 import Imagen7 from './Imagenes Galeria/Imagen 7.png'
 import Imagen8 from './Imagenes Galeria/Imagen 8.jpeg'
 import Imagen9 from './Imagenes Galeria/Imagen 9.jpeg'
+import Imagen10 from './Imagenes Galeria/Imagen 10.jpg'
+import Imagen11 from './Imagenes Galeria/Imagen 11.jpg'
+import Imagen12 from './Imagenes Galeria/Imagen 12.png'
+import Imagen13 from './Imagenes Galeria/Imagen 13.jpg'
+import Imagen14 from './Imagenes Galeria/Imagen 14.jpeg'
+import Imagen15 from './Imagenes Galeria/Imagen 15.jpg'
 
 export function Galeria () {
     return (
@@ -22,6 +28,12 @@ export function Galeria () {
             <div><img src={Imagen7} alt="" /></div>
             <div><img src={Imagen8} alt="" /></div>
             <div><img src={Imagen9} alt="" /></div>
+            <div><img src={Imagen10} alt="" /></div>
+            <div><img src={Imagen11} alt="" /></div>
+            <div><img src={Imagen12} alt="" /></div>
+            <div><img src={Imagen13} alt="" /></div>
+            <div><img src={Imagen14} alt="" /></div>
+            <div><img src={Imagen15} alt="" /></div>
         </div>
     )
 }
